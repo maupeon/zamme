df94dbfd2abbca2fc7cbb32b0827e0c419436d77
maupeon <maupeon@gmail.com>
2023-09-09 10:42:06 -0600

    Added more events and the moving bar

--- components/Events.js
+++ components/Events.js
@@ -2,15 +2,200 @@ import React from "react";
 
 const events_array = [
   {
+    type: "Zamme x Etceteras Book Fair",
+    // authors: " + ",
+    authors: [
+      {
+        name: "Festival feminista de design e edição",
+        link: "https://www.etceteras.net/",
+      },
+    ],
+    date: "05–07.10.23 (PT)",
+    title: "",
+  },
+  {
+    type: "Zamme workshop",
+    // authors: " + ",
+    authors: [
+      {
+        name: "Fernanda Ruíz",
+        link: "https://www.fernandaruiz.work/",
+      },
+    ],
+    date: "09,11–12.09.23 (MX)",
+    title: "Building your digital garden: creative coding",
+  },
+  {
+    type: "Zamme x ee14",
+    // authors: " + ",
+    authors: [
+      {
+        name: "En el 14",
+        link: "https://www.instagram.com/en_el_14/",
+      },
+      {
+        name: "Ted Davis",
+        link: "https://teddavis.org/xyscope/",
+      },
+    ],
+    date: "05.08.23 (MX)",
+    title: "Summer Social",
+  },
+  {
+    type: "Zamme talk ",
+    // authors: " + ",
+    authors: [
+      {
+        name: "Fernada Ruíz",
+        link: "https://www.fernandaruiz.work/",
+      },
+    ],
+    date: "20.07.23 (MX)",
+    title:
+      "Digital design: variables & approaches to a digital design practice",
+  },
+  {
+    type: "Zamme workshop",
+    // authors: " + ",
+    authors: [
+      {
+        name: "Can Can Press",
+        link: "https://cancanpress.com/",
+      },
+      {
+        name: "SUPERCONTINENTE",
+        link: "https://supercontinente.com/",
+      },
+    ],
+    date: "24–25.06.23 (MX)",
+    title: "Taller de tipografía",
+  },
+  {
+    type: "Zamme talk",
+    // authors: " + ",
+    authors: [
+      {
+        name: "Fabiola Mejía",
+        link: "https://supercontinente.com/",
+      },
+    ],
+    date: "22. 06.23 (MX)",
+    title: "Typeface design and collaborative exploration",
+  },
+  {
+    type: "Zamme talk",
+    // authors: " + ",
+    authors: [
+      {
+        name: "Alfonso de Anda",
+        link: "https://www.aldeanda.com/",
+      },
+      {
+        name: "David Rocha",
+        link: "https://davidrochart.bigcartel.com/product/evite-obedecer",
+      },
+      {
+        name: "Mariana de Alba",
+        link: "https://www.instagram.com/marianadalba/",
+      },
+    ],
+    date: "25.05.23 (MX)",
+    title: "Creative process and multidisciplinary practice",
+  },
+  {
+    type: "Zamme talk",
+    // authors: " + ",
+    authors: [
+      {
+        name: "Sophie Greenspan",
+        link: "https://www.sophiegreenspan.com/",
+      },
+      {
+        name: "Aldo Solano",
+        link: "https://www.academia.edu/68691731/Playgrounds_del_M%C3%A9xico_moderno",
+      },
+    ],
+    date: "18.04.23 (MX)",
+    title: "Visual research on urban spaces",
+  },
+  {
+    type: "Zamme workshop",
+    // authors: " + ",
+    authors: [
+      {
+        name: "Can Can Press",
+        link: "https://cancanpress.com/",
+      },
+      {
+        name: "Dan Solbach",
+        link: "https://dansolbach.ch/",
+      },
+    ],
+    date: "01.04.23 (MX)",
+    title: "Taller de diseño de cartel",
+  },
+  {
+    type: "Zamme talk",
+    // authors: " + ",
+    authors: [
+      {
+        name: "Estudio Herrera",
+        link: "https://estudioherrera.mx/#jul-20-23-mx182406",
+      },
+    ],
+    date: "31.03.23 (MX)",
+    title: "Design process and studio dynamics",
+  },
+  {
+    type: "Zamme Berlin",
+    // authors: " + ",
+    authors: [
+      { name: "clhub.art", link: "https://www.clhub.art" },
+      { name: "Wex Records", link: "https://wexrecords.bandcamp.com" },
+    ],
+    date: "07.09.22 (DE)",
+    title: "",
+  },
+  {
+    type: "Zamme talk + live painting",
+    // authors:'María Marín de Buen & Dan Solbach',
+    authors: [{ name: "Memo Vithana", link: "https://www.memovithana.com/" }],
+    date: "07.08.22 (DE)",
+    title: "",
+  },
+  {
+    type: "En casa con",
+    // authors:'María Marín de Buen & Dan Solbach',
+    authors: [{ name: "Carlo Canún", link: "https://carlocanun.com" }],
+    date: "06.08.22 (MX)",
+    title: "",
+    cohost: "Emilio Ferrer",
+    link: "https://www.emilioerandi.com/",
+  },
+  {
     type: "Zamme talk",
     // authors:'María Marín de Buen & Dan Solbach',
     authors: [
-      { name: "María Marín de Buen", link: "https://mariamarindebuen.com" },
-      { name: "Dan Solbach", link: "https://dansolbach.ch" },
+      { name: "Liz Zepeda Esquivel", link: "https://lizdesigns.co" },
+      { name: "Santiago Martínez", link: "https://www.suave.ooo/shop" },
     ],
-    date: "25.03.22 (MX)",
-    title: "Graphic design for artists & cultural institutions",
+    date: "15.06.22 (MX)",
+    title: "Creative direction for magazines",
   },
+  {
+    type: "Zamme talk",
+    // authors:'María Marín de Buen & Dan Solbach',
+    authors: [
+      {
+        name: "Andrea García",
+        link: "https://miauediciones.wixsite.com/miauxnohacernada",
+      },
+      { name: "Jackie Crespo", link: "http://cancanpress.com" },
+    ],
+    title: "Graphic design & self-publishing",
+    date: "21.05.22 (MX)",
+  },
+
   {
     type: "Zamme talk",
     // authors:'María Marín de Buen & Dan Solbach',
@@ -27,50 +212,11 @@ const events_array = [
     type: "Zamme talk",
     // authors:'María Marín de Buen & Dan Solbach',
     authors: [
-      {
-        name: "Andrea García",
-        link: "https://miauediciones.wixsite.com/miauxnohacernada",
-      },
-      { name: "Jackie Crespo", link: "http://cancanpress.com" },
+      { name: "María Marín de Buen", link: "https://mariamarindebuen.com" },
+      { name: "Dan Solbach", link: "https://dansolbach.ch" },
     ],
-    title: "Graphic design & self-publishing",
-    date: "21.05.22 (MX)",
-  },
-  {
-    type: "Zamme talk",
-    // authors:'María Marín de Buen & Dan Solbach',
-    authors: [
-      { name: "Liz Zepeda Esquivel", link: "https://lizdesigns.co" },
-      { name: "Santiago Martínez", link: "https://www.suave.ooo/shop" },
-    ],
-    date: "15.06.22 (MX)",
-    title: "Creative direction for magazines",
-  },
-  {
-    type: "En casa con",
-    // authors:'María Marín de Buen & Dan Solbach',
-    authors: [{ name: "Carlo Canún", link: "https://carlocanun.com" }],
-    date: "06.08.22 (MX)",
-    title: "",
-    cohost: "Emilio Ferrer",
-    link: "https://www.emilioerandi.com/",
-  },
-  {
-    type: "Zamme talk + live painting",
-    // authors:'María Marín de Buen & Dan Solbach',
-    authors: [{ name: "Memo Vithana", link: "https://www.memovithana.com/" }],
-    date: "07.08.22 (DE)",
-    title: "",
-  },
-  {
-    type: "Zamme Berlin",
-    // authors: " + ",
-    authors: [
-      { name: "clhub.art", link: "https://www.clhub.art" },
-      { name: "Wex Records", link: "https://wexrecords.bandcamp.com" },
-    ],
-    date: "07.09.22 (DE)",
-    title: "",
+    date: "25.03.22 (MX)",
+    title: "Graphic design for artists & cultural institutions",
   },
 ];
 
@@ -91,7 +237,7 @@ export default function Events() {
 
   return (
     <div className=" font-zamme_light">
-      <h1 className=" mb-8 text-lg">Past events</h1>
+      <h1 className=" mb-8 text-lg">Events</h1>
       {events_array.map((item, index) => (
         <div className=" mb-8" key={index}>
           <p>{item.type}</p>
--- components/MovingBar.js
+++ components/MovingBar.js
@@ -5,12 +5,14 @@ export default function MovingBar() {
     <div className=" font-zamme_light text-lg">
       <div className="  flex fixed inset-0 bg-white w-full  h-10 ">
         <div className="  animate-scroll-left   marquee-group shrink-0 flex h-full w-full items-center">
-          <div className=" shrink-0 flex text-justify mr-5">Próximamente</div>
-          <div className=" shrink-0 flex text-justify mr-5">Coming Soon</div>
+          <div className=" shrink-0 flex text-justify mr-20">Próximamente</div>
+          <div className=" shrink-0 flex text-justify mr-20">Coming Soon</div>
+          <div className=" shrink-0 flex text-justify mr-20">WIP 4L</div>
         </div>
         <div className=" animate-scroll-left  marquee-group shrink-0 flex h-full w-full items-center">
-          <div className=" shrink-0 flex text-justify mr-5">Próximamente</div>
-          <div className=" shrink-0 flex text-justify mr-5">Coming Soon</div>
+          <div className=" shrink-0 flex text-justify mr-20">Próximamente</div>
+          <div className=" shrink-0 flex text-justify mr-20">Coming Soon</div>
+          <div className=" shrink-0 flex text-justify mr-20">WIP 4L</div>
         </div>
       </div>
     </div>
